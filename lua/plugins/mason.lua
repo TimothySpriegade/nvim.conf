50328127d6e28dffff06bef907727c62f2074525
Timothy Spriegade <timothyspriegade@outlook.de>
2025-10-02 20:04:42 +0200

    removed cord and added neovim-lspconfig for senseable default lsp settings

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,22 +1,25 @@
 return {
-    {
-    "mason-org/mason.nvim",
-    config = function ()
-        require("mason").setup()
-    end
-    },
-    {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "gopls", 
-                "lua_ls", 
-                "autotools_ls", 
-                "bashls", 
-                "docker_language_server"
-            }
-        })
-    end
-    }
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"gopls",
+					"lua_ls",
+					"autotools_ls",
+					"bashls",
+					"docker_language_server"
+				}
+			})
+		end
+	},
+	{
+		"neovim/nvim-lspconfig"
+	}
 }
