142cc4dd238b4163d9e4f1b900a0310d09082df4
Timothy Spriegade <timothyspriegade@outlook.de>
2025-10-02 20:16:27 +0200

    added nightfox theme variants

--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -1,29 +1,35 @@
 return {
-  "zaldih/themery.nvim",
-  lazy = false,
-  config = function()
-    require("themery").setup({
-      themes = {
-        { name = "nightfox", colorscheme = "nightfox" },
-        { name = "tokyonight-day", colorscheme = "tokyonight-day" },
-        { name = "tokyonight-night", colorscheme = "tokyonight-night" },
-        { name = "tokyonight-storm", colorscheme = "tokyonight-storm" },
-        { name = "tokyonight-moon", colorscheme = "tokyonight-moon" },
-        { name = "github_dark", colorscheme = "github_dark" },
-        { name = "github_dark_default", colorscheme = "github_dark_default" },
-        { name = "github_dark_dimmed", colorscheme = "github_dark_dimmed" },
-        { name = "github_dark_high_contrast", colorscheme = "github_dark_high_contrast" },
-        { name = "github_dark_colorblind", colorscheme = "github_dark_colorblind" },
-        { name = "github_dark_tritanopia", colorscheme = "github_dark_tritanopia" },
-        { name = "github_light", colorscheme = "github_light" },
-        { name = "github_light_default", colorscheme = "github_light_default" },
-        { name = "github_light_high_contrast", colorscheme = "github_light_high_contrast" },
-        { name = "github_light_colorblind", colorscheme = "github_light_colorblind" },
-        { name = "github_light_tritanopia", colorscheme = "github_light_tritanopia" },
-        { name = "vague", colorscheme = "vague" },
-        { name = "vscode", colorscheme = "vscode" },
-      },
-    })
-  end,
-  vim.keymap.set("n", "<leader>T", "<cmd>Themery<CR>", {desc = "Toggle Themerey"})
+	"zaldih/themery.nvim",
+	lazy = false,
+	config = function()
+		require("themery").setup({
+			themes = {
+				{ name = "Nightfox",                   colorscheme = "nightfox" },
+				{ name = "Dayfox",                     colorscheme = "dayfox" },
+				{ name = "Dawnfox",                    colorscheme = "dawnfox" },
+				{ name = "Duskfox",                    colorscheme = "duskfox" },
+				{ name = "Nordfox",                    colorscheme = "nordfox" },
+				{ name = "Terafox",                    colorscheme = "terafox" },
+				{ name = "Carbonfox",                  colorscheme = "carbonfox" },
+				{ name = "tokyonight-day",             colorscheme = "tokyonight-day" },
+				{ name = "tokyonight-night",           colorscheme = "tokyonight-night" },
+				{ name = "tokyonight-storm",           colorscheme = "tokyonight-storm" },
+				{ name = "tokyonight-moon",            colorscheme = "tokyonight-moon" },
+				{ name = "github_dark",                colorscheme = "github_dark" },
+				{ name = "github_dark_default",        colorscheme = "github_dark_default" },
+				{ name = "github_dark_dimmed",         colorscheme = "github_dark_dimmed" },
+				{ name = "github_dark_high_contrast",  colorscheme = "github_dark_high_contrast" },
+				{ name = "github_dark_colorblind",     colorscheme = "github_dark_colorblind" },
+				{ name = "github_dark_tritanopia",     colorscheme = "github_dark_tritanopia" },
+				{ name = "github_light",               colorscheme = "github_light" },
+				{ name = "github_light_default",       colorscheme = "github_light_default" },
+				{ name = "github_light_high_contrast", colorscheme = "github_light_high_contrast" },
+				{ name = "github_light_colorblind",    colorscheme = "github_light_colorblind" },
+				{ name = "github_light_tritanopia",    colorscheme = "github_light_tritanopia" },
+				{ name = "vague",                      colorscheme = "vague" },
+				{ name = "vscode",                     colorscheme = "vscode" },
+			},
+		})
+	end,
+	vim.keymap.set("n", "<leader>T", "<cmd>Themery<CR>", { desc = "Toggle Themerey" })
 }
